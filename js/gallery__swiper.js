027cb1eea5cdd68771350d0494da3ec26029ddca
Андрей Соловьев <gmail.com>
2021-12-07 13:12:11 +0700

    add gallery

---
+++ js/gallery__swiper.js
@@ -0,0 +1,71 @@
+let gallerySlider = new Swiper(".swiper", {
+  slidesPerView: 1,
+  grid: {
+    rows: 1,
+    fill: "row",
+  },
+  spaceBetween: 20,
+  pagination: {
+    el: ".gallery .gallery__pagination",
+    type: "fraction",
+  },
+  navigation: {
+    nextEl: ".gallery__next",
+    prevEl: ".gallery__prev",
+  },
+
+  breakpoints: {
+    441: {
+      slidesPerView: 2,
+      grid: {
+        rows: 2,
+      },
+      spaceBetween: 30,
+    },
+
+    1200: {
+      slidesPerView: 3,
+      grid: {
+        rows: 2,
+      },
+      spaceBetween: 50,
+    },
+  },
+
+  a11y: false,
+  keyboard: true, // можно управлять с клавиатуры стрелками влево/вправо
+
+  // Дальнейшие надстройки делают слайды вне области видимости не фокусируемыми
+  watchSlidesProgress: true,
+  slideVisibleClass: "slide-visible",
+
+  on: {
+    init: function () {
+      this.slides.forEach((slide) => {
+        if (!slide.classList.contains("slide-visible")) {
+          slide.tabIndex = "-1";
+        } else {
+          slide.tabIndex = "";
+        }
+      });
+    },
+    slideChange: function () {
+      this.slides.forEach((slide) => {
+        if (!slide.classList.contains("slide-visible")) {
+          slide.tabIndex = "-1";
+        } else {
+          slide.tabIndex = "";
+        }
+      });
+    },
+  },
+
+  // on: {
+  //   /* исправляет баг с margin-top остающимся при смене брейкпоинта, это было нужно в 6-й версии свайпера */
+  //   beforeResize: function () {
+  //     this.slides.forEach((el) => {
+  //       el.style.marginTop = "";
+  //     });
+  //   }
+  // }
+});
